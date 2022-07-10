81728f17aabeeca250ea364e0cae2f393cef101c
AngryWeather <WerRic96@protonmail.com>
2022-07-10 15:51:43 +0200

    first config

---
+++ core/src/com/github/angryweather/pong01/Pong01.java
@@ -0,0 +1,25 @@
+package com.github.angryweather.pong01;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Pong01 extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
