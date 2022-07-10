624abe4246a654df3e9ec5643bc066a9be079595
AngryWeather <WerRic96@protonmail.com>
2022-07-10 16:57:52 +0200

    created a camera

--- core/src/com/github/angryweather/pong01/Pong01.java
+++ core/src/com/github/angryweather/pong01/Pong01.java
@@ -1,6 +1,7 @@
 package com.github.angryweather.pong01;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -10,14 +11,19 @@ public class Pong01 extends ApplicationAdapter {
 	static final int SCREEN_WIDTH = 1280;
 	static final int SCREEN_HEIGHT = 720;
 
+	OrthographicCamera camera;
+
+
 	@Override
 	public void create () {
-
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1280, 720);
 	}
 
 
 	@Override
 	public void render () {
+		camera.update();
 		ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1);
 
 	}
