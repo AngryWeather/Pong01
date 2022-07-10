8b1bf47e5c49f23e8365f6428ccf6f498e6b8dfd
AngryWeather <WerRic96@protonmail.com>
2022-07-10 16:55:17 +0200

    changed background color

--- core/src/com/github/angryweather/pong01/Pong01.java
+++ core/src/com/github/angryweather/pong01/Pong01.java
@@ -6,15 +6,19 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Pong01 extends ApplicationAdapter {
-	
+
+	static final int SCREEN_WIDTH = 1280;
+	static final int SCREEN_HEIGHT = 720;
+
 	@Override
 	public void create () {
 
 	}
 
+
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1);
 
 	}
 	
