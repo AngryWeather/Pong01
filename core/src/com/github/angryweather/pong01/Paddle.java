91ae4bd597b25fd57b25d53da650c2177755a882
AngryWeather <WerRic96@protonmail.com>
2022-07-11 21:15:45 +0200

    added width and height for a paddle

--- core/src/com/github/angryweather/pong01/Paddle.java
+++ core/src/com/github/angryweather/pong01/Paddle.java
@@ -1,2 +1,6 @@
-package com.github.angryweather.pong01;public class Paddle {
+package com.github.angryweather.pong01;
+
+public class Paddle {
+    final int width = 20;
+    final int height = 80;
 }
