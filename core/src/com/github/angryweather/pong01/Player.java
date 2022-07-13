cf237e374d3501e9b467f74e78ba87fac80fce65
AngryWeather <WerRic96@protonmail.com>
2022-07-13 17:35:02 +0200

    refactored Player class

--- core/src/com/github/angryweather/pong01/Player.java
+++ core/src/com/github/angryweather/pong01/Player.java
@@ -2,13 +2,34 @@ package com.github.angryweather.pong01;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
-public abstract class Player {
+public class Player {
     ShapeRenderer shapeRenderer;
-    int score = 0;
+    private Paddle paddle = new Paddle();
+    private int score = 0;
+    private float x;
+    private float y;
 
 
-    public Player(ShapeRenderer shapeRenderer) {
+    public Player(ShapeRenderer shapeRenderer, float x, float y) {
         this.shapeRenderer = shapeRenderer;
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
     }
 
     public int getScore() {
@@ -17,5 +38,7 @@ public abstract class Player {
     public void setScore(int score) {
         this.score = score;
     }
-    public abstract void render();
+    public void render() {
+        shapeRenderer.rect(x, y, paddle.getWidth(), paddle.getHeight());
+    }
 }
